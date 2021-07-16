4599479c49562c0390587e4714bf6950365a792a
Zytekaron <zytekaron@gmail.com>
2021-07-16 01:18:13 -0700

    example/test update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"access-control/src/perms"
 	"access-control/src/rss"
+	"access-control/src/status"
 	"access-control/src/types"
 	"fmt"
 	"time"
@@ -11,13 +12,18 @@ import (
 func main() {
 	var p rss.Permission = &rss.Terminal{
 		Group: &rss.Group{
-			ID:          "group_1",
+			ID: "group_1",
+			Permissions: []rss.Permission{
+				&perms.AllowLevel{Level: 10}, // all of these are ignored when override is present
+				&perms.AllowAnyRole{Roles: []string{"staff"}},
+				&perms.DenyAnyRole{Roles: []string{"stupid"}},
+			},
 			Overwrite: &rss.Group{
 				ID: "group_1_overwrite",
 				Permissions: []rss.Permission{
-					&perms.AllowLevel{Level: 11}, // Level 11 or staff, but not if you have stupid_key
-					&perms.AllowAnyRole{Roles: []string{"staff"}},
-					&perms.DenyAnyRole{Roles: []string{"stupid_key"}},
+					&perms.AllowLevel{Level: 11}, // level 11 or staff, but not if you have the o_stupid role
+					&perms.AllowAnyRole{Roles: []string{"o_staff"}},
+					&perms.DenyAnyRole{Roles: []string{"o_stupid"}},
 				},
 			},
 		},
@@ -30,11 +36,15 @@ func main() {
 			Roles:    []string{"staff"},
 			Disabled: false,
 		},
-		Roles:    []string{"super_key"},
+		Roles:    []string{"staff", "stupid", "o_st4aff"}, // only o_staff matters when an override is present
 		Disabled: false,
 		Expires:  types.JsonTime(time.Now().Add(time.Hour * 10000)),
 	})
-	fmt.Println(ok.Status, ok.Reason)
+	// ternary 100
+	stat := map[status.Status]string{
+		status.Allow:   "Allow",
+		status.Neutral: "Neutral",
+		status.Deny:    "Deny",
+	}[ok.Status]
+	fmt.Println(stat, "|", ok.Reason)
 }
-
-// Key#Use
